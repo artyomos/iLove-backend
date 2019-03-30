c3605f88a725440afb27fcbbbf3f388fd3dbe4cd
Nate R <nate.ruppert@csu.fullerton.edu>
2019-03-30 15:44:31 -0700

    added exceptions/proper logic

--- api/database.py
+++ api/database.py
@@ -1,9 +1,14 @@
 #Database test
 
 from time import time
+import random, string
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from flask import jsonify
+
+class NoInput(Exception):
+    pass
 
 # Use the application default credentials
 cred = credentials.ApplicationDefault()
@@ -16,24 +21,36 @@ db = firestore.client()
 #Problem: Users with same name will cause overwrite of data.
 def addUser(name):
     """
-    Add User/Change data in User Class
+    Add User
     """
     doc_ref = db.collection(u'users').document()
     key = doc_ref.id
+
+    print("Created Reference for user: {0} [Key: {1}]".format(name, key))
+    
     data = {
         u'name': name,
         u'creation_date': time(),
         u'user_likes': {}
     }
-    print("Created Reference for user: {0} [Key: {1}]".format(name, key))
-    request = doc_ref.set(data)
-    return key
+    doc_ref.set(data)
 
-addUser('Apple')
+    package = {
+        "id" : key
+    }
+    return jsonify(package)
+
 def main(requests):
     if requests:
-        print("DEBUG: Found Request")
         json = requests.get_json()
         if 'name' in json:
-            print("DEBUG: Processing Request")
-            return addUser(json['name'])
+            name = json['name']
+            if name:
+                print("DEBUG: Processing Request")
+                return addUser()
+            else:
+                #TODO make status code response instead
+                raise NoInput("Name was Empty in Request")
+    else:
+        #TODO make status code response instead
+        raise NoInput("Header was Empty in Request")
