cdbd57b9adf041a21a9177f29ff4eb268b9e263e
Nate R <nate.ruppert@csu.fullerton.edu>
2019-03-30 16:29:13 -0700

    added organization to app.py

--- api/app.py
+++ api/app.py
@@ -1,27 +1,35 @@
-#Database test
-
-from flask import Flask
-
+# Local Imports / Files
 from user import create
 
+# Flask App
+from flask import Flask
 app = Flask(__name__)
 
+#TODO Remove/Update this
+# Temporary Exception for failed creation of Users
 class NoInput(Exception):
     pass
 
+### PAGE CALLS ###
+
+# Main Page Directory
 @app.route("/")
 def hewwo():
     return 'Hewwo! ^w^'
 
+# Login Page
 @app.route("/login")
 def login():
     pass
 
+# User Pages
 @app.route("/user/<user_id>")
 def show_user(user_id):
     pass
 
+### API CALLS ###
 
+# Create User Function
 @app.route("/api/user/create")
 def create_user(requests):
     if requests:
@@ -38,9 +46,10 @@ def create_user(requests):
         #TODO make status code response instead
         raise NoInput("Header was Empty in Request")
 
-#Test Function
+### Flask Testing Function ###
 def test():
     with app.app_context():
         create.addUser("OwO")
 
+# Invoke of Tester
 test()
