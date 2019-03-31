da796a009ee6e0ddf7c68747311e2732730e7245
Nate R <nate.ruppert@csu.fullerton.edu>
2019-03-30 17:28:36 -0700

    added remove_interest

--- api/user/interests.py
+++ api/user/interests.py
@@ -38,3 +38,29 @@ def add_interest(args):
         'success':True
     }
     return jsonify(package)
+
+def remove_interest(args):
+    """
+    Remove User Interest
+    """
+    doc_ref = database.db.collection(u'users').document(args['user'])
+
+    # Ensure proper ID was grabbed
+    assert(doc_ref.id == args['user'])
+
+    data = doc_ref.get().to_dict()
+    previous_interests = data['user_likes']
+    print("DEBUG: Previous Interests are {0}".format(previous_interests))
+
+    # Remove Interest
+    del previous_interests[args['interest']['name']]
+
+    doc_ref.update(data)
+
+    print("DEBUG: New Interests are {0}".format(previous_interests))
+
+    # If reached this point, all was successful
+    package = {
+        'success':True
+    }
+    return jsonify(package)
